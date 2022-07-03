f5361121be339856696af1bd6edfbf063d904d13
ayoub rezaei <ayoub.frontend1992@gmail.com>
2022-07-03 04:42:53 -0700

    finish shop with react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import { Route,Routes,Navigate, } from 'react-router-dom';
 import './App.css';
-
-
-
+import { Route, Navigate, Routes } from "react-router-dom";
 
 // Components
 import Store from './components/Store';
-import ProductDetail from './components/ProductDetail';
+import ProductDetails from './components/ProductDetails';
+import Navbar from './components/shared/Navbar';
+import ShopCart from './components/ShopCart';
 
 // Context
 import ProductContextProvider from './context/ProductContextProvider';
-import CartContextProvider from './context/CartContextProvider.js';
+import CartContextProvider from './context/CartContextProvider';
 
 function App() {
   return (
     <ProductContextProvider>
       <CartContextProvider>
-      <Routes>
-        <Route path='/products/' element={<Store/>} />
-        <Route path='/products/:id' element={<ProductDetail/>} />
-        <Route path='/' element={<Navigate to="/products" />} />
-      </Routes>
+        <Navbar />
+        <Routes>
+          <Route path="/products/:id" element={<ProductDetails/>} />
+          <Route path="/products" element={<Store/>} />
+          <Route path="/cart" element={<ShopCart/>} />
+          <Route path="/" element={<Navigate to="/products" />} />
+        </Routes>
       </CartContextProvider>
     </ProductContextProvider>
   );
--- src/context/CartContextProvider.js
+++ src/context/CartContextProvider.js
@@ -7,8 +7,14 @@ const initialState = {
     checkout: false
 }
 
+const sumItems = items => {
+    const itemsCounter = items.reduce((total, product) => total + product.quantity, 0);
+    let total = items.reduce((total, product) => total + product.price * product.quantity, 0).toFixed(2);
+    return {itemsCounter, total}
+}
+
 const cartReducer = (state, action) => {
-    console.log(state)
+    console.log(action.type)
     switch(action.type) {
         case "ADD_ITEM":
             if (!state.selectedItems.find(item => item.id === action.payload.id)) {
@@ -19,25 +25,33 @@ const cartReducer = (state, action) => {
             }
             return {
                 ...state,
-                selectedItems: [...state.selectedItems]
+                selectedItems: [...state.selectedItems],
+                ...sumItems(state.selectedItems),
+                checkout: false
             }
         case "REMOVE_ITEM":
             const newSelectedItems = state.selectedItems.filter(item => item.id !== action.payload.id);
             return {
                 ...state,
-                selectedItems: [...newSelectedItems]
+                selectedItems: [...newSelectedItems],
+                ...sumItems(newSelectedItems)
+
             }
         case "INCREASE":
             const indexI = state.selectedItems.findIndex(item => item.id === action.payload.id);
             state.selectedItems[indexI].quantity++;
             return {
                 ...state,
+                ...sumItems(state.selectedItems)
+
             }
         case "DECREASE":
             const indexD = state.selectedItems.findIndex(item => item.id === action.payload.id);
             state.selectedItems[indexD].quantity--;
             return {
                 ...state,
+                ...sumItems(state.selectedItems)
+
             }
         case "CHECKOUT" :
             return {
@@ -54,7 +68,7 @@ const cartReducer = (state, action) => {
                 checkout: false
             }
         default: 
-        return state;
+            return state;
     }   
 }
 
